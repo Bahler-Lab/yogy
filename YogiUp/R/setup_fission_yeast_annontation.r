33d2bd9abff97780c0e7da459fd1b17961cde47c
sinanshi <shisinancn@gmail.com>
2015-11-09 12:49:50 +0000

    setup fission yeast

--- YogiUp/R/setup_fission_yeast_annontation.r
+++ YogiUp/R/setup_fission_yeast_annontation.r
@@ -7,84 +7,131 @@
 #    Sinan Shi       05-11-2015
 #======================================================================
 
-
-#add_matching_column<-function(table, prim, new_vec, new_col_name, vec_col, col_value){
-#    index_vector<-match(new_vec[, vec_col], prim)
-#    index_table<-
-#    index_new<-
-#    chosen_row<-unique(match(match_array[, match_col], gene_name))
-#    tablematch_array[chosen_row, var_col]
-#    return
-#
-#}
-#
+#' check if a vector has duplicated entries. 
+is.unique<-function(vec){
+    vec<-vec[vec!=""]
+    if(length(vec) == length(unique(vec))) return(TRUE)
+    else  return(FALSE)
+}
 
 #' get_unique_table
-#' @param filename filename
-#' @param unique_col the column to match
-get_unique_table<-function(filename, unique_col, makeUnique=TRUE, ...){
-    table<-read.delim(filename, stringsAsFactors=F, sep="\t", quote="", ...)
-    table<-table[table[, unique_col] != "", ]
-    if(makeUnique){
-        table<-table[!duplicated(table[, unique_col]), ]# remove duplicated rows
+#' @param input table
+#' @param unique_col the column that requires to be unique
+#' @return table that has duplicated rows removed
+make_unique_table<-function(table, unique_col){
+    this.col<-table[, unique_col]
+    if(is.unique(this.col)){
+        return(table)
     }else{
-        stopifnot(length(table[, unique_col]) == length(unique(table[, unique_col])))
+        uni_table<-table[!duplicated(this.col)|this.col=="", ]
+        return(uni_table)
     }
-    return(table)
 }
 
 
-match_table<-function(table_base, table_match, col_base, col_match, col_val){
-    index<-match(table_base[, col_base], table_match[, col_match] )
+
+match_table<-function(base, table_match, col_match, col_val, ...){
+    index<-match(base, table_match[, col_match], ...)
+    index<-index[!is.na(index)]
     new<-table_match[index, col_val] 
     return(new)
 }
 
 
+#' read following three tables
+#' header information are found on the website.
+#' allName.tsv           -   no header, skip 2 lines
+#' PomBase2UniPort.tsv   -   with header
+#' sysID2product.tsv     -   no header, skip 2 lines
+get.unique.from.rawtables<-function(){
+
+    header_allname<-c("systematic_name", "primary_name", "synonyms")
+    header_uniport<-c("stable_id", "dbprimary_acc")
+    header_product<-c("systematic_name", "primary_name", "synonyms", "product")
+    #read table
+    t_allname<<-read.delim("temp/allName.tsv", col.name=header_allname,
+                           header=FALSE, skip=2, stringsAsFactors=FALSE, sep="\t",
+                           quote="")
+    t_product<<-read.delim("temp/sysID2product.tsv", col.name=header_product,
+                           header=FALSE, skip=2, stringsAsFactors=FALSE, sep="\t",
+                           quote="")
+    t_uniport<<-read.delim("temp/PomBase2UniPort.tsv", stringsAsFactors=FALSE, sep="\t",
+                           quote="")
+
+    # make sure that all tables are has unique entries.
+    t_allname<<-make_unique_table(t_allname, "systematic_name")
+    t_allname<<-make_unique_table(t_allname, "primary_name")
+
+    t_product<<-make_unique_table(t_product, "systematic_name")
+
+    t_uniport<-make_unique_table(t_uniport, "stable_id")
+    t_uniport<<-make_unique_table(t_uniport, "dbprimary_acc")
+}
 
 
-create_fission_yeats_table<-function(t_allname, t_uniport, t_product){
-    nrow<-length(t_allname[, 1])
-    fission_table<-data.frame("systematic_name"=t_allname[,"primary_name"], 
-                              "ec_number"=rep("", nrow),
-                              "PombePD"=match_table(t_allname, "primary_name", 
-                                                    t_product, "primary_name", "product"),
-                              "Our_annotation"=rep("", nrow),
-                              "Phenotype"=rep("", nrow),
-                              "Function"=rep("", nrow),
-                              "Keywords"=rep("", nrow),
-                              "PubMedID"=        "(systematic id)",
-                              "GeneDB"=         "(description)",
-                              "Remarks"=rep("", nrow),
-                              "Swissprot"=          (uniprotid),
-                              "Type"=rep("", nrow),
-                              "DBid"=rep("", nrow),
-                              "GO_biological_process"=rep("", nrow),
-                              "GO_molecular_function"=rep("", nrow),
-                              "GO_cellular_component"=rep("", nrow),
-                              "RefSeq"=rep("", nrow),
-                              "UniGene"=rep("", nrow))
+# the new table will only extract 4 information from downloaded tables,
+# 1. systematic name, 2. fussed systematic and primary name, 3. description
+# 4. uniport. Duplicated primary names and systematic names have been 
+# removed. 
+get.new.table<-function(){
+    select_systematic_name<-match_table(t_uniport[, "stable_id"], t_allname,
+                                        "systematic_name", "systematic_name")
+
+    select_primary_name<-match_table(select_systematic_name, t_allname,
+                                     "systematic_name", "primary_name")
+    # fuse the systematic name and primary name
+    select_primary_name[select_primary_name == ""] <-
+        select_systematic_name[select_primary_name == ""]
+
+
+    select_uniport<-match_table(select_systematic_name, t_uniport,
+                                "stable_id", "dbprimary_acc")
+    select_description<-match_table(select_systematic_name, t_product,
+                                    "systematic_name", "product", nomatch="")
+
+    # check
+    stopifnot(length(select_primary_name) == length(select_systematic_name))
+    stopifnot(length(select_primary_name) == length(select_uniport))
+    stopifnot(length(select_primary_name) == length(select_description))
+    return(list("primary_name" = select_primary_name, 
+                "systematic_name" = select_systematic_name,
+                "description" = select_description, 
+                "uniport" = select_uniport))
+
 }
 
 
 
-# header information are found on the website.
-# allName.tsv           -   no header, skip 2 lines
-# PomBase2UniPort.tsv   -   with header
-# sysID2product.tsv     -   no header, skip 2 lines
+#' create fission table
+create.fission.yeats.table<-function(new_table){
+    nrow<-length(new_table[[1]])
+    fission_table<-data.frame("systematic_name"       = new_table[["primary_name"]], 
+                              "ec_number"             = rep("", nrow),
+                              "PombePD"               = new_table[["description"]],
+                              "Our_annotation"        = rep("", nrow),
+                              "Phenotype"             = rep("", nrow),
+                              "Function"              = rep("", nrow),
+                              "Keywords"              = rep("", nrow),
+                              "PubMedID"              = new_table[["systematic_name"]],
+                              "GeneDB"                = new_table[["description"]],
+                              "Remarks"               = rep("", nrow),
+                              "Swissprot"             = new_table[["uniport"]],
+                              "Type"                  =rep("", nrow),
+                              "DBid"                  =rep("", nrow),
+                              "GO_biological_process" =rep("", nrow),
+                              "GO_molecular_function" =rep("", nrow),
+                              "GO_cellular_component" =rep("", nrow),
+                              "RefSeq"                =rep("", nrow),
+                              "UniGene"               =rep("", nrow))
+    return(fission_table)
+}
 
-header_allname<-c("systematic_name", "primary_name", "synonyms")
-header_uniport<-c("stable_id", "dbprimary_acc")
-header_product<-c("systematic_name", "primary_name", "synonyms", "product")
 
-t_allname<-get_unique_table("temp/allName.tsv", unique_col="primary_name",
-                            col.names=header_allname, 
-                            header=F, skip=2) 
-t_product<-get_unique_table("temp/sysID2product.tsv", unique_col="primary_name",
-                             col.names=header_product,
-                            header=F, skip=2)
-t_uniport<-get_unique_table("temp/PomBase2UniPort.tsv", unique_col="stable_id")
-
-
-
-
+#--------------------------------
+# Main
+#--------------------------------
+get.unique.from.rawtables()
+new_table<-get.new.table()
+fission_table<-create.fission.yeats.table(new_table)
+write.table(fission_table, "data/Fission_yeas_annotations_db.txt", 
+            quote=FALSE, sep="\t", row.name=FALSE, col.name=FALSE)
