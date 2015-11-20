18e1b31d7ef8953657c5dbd5f2681b818e4fd1c7
sinanshi <shisinancn@gmail.com>
2015-11-20 14:53:26 +0000

    inparnoid update

--- YogiUp/update.sh
+++ YogiUp/update.sh
@@ -11,14 +11,23 @@ HOSTNAME=localhost
 USERNAME=yogyrw
 PASSWORD=yogyex
 PORT=3306
+MYSQL=/usr/local/Cellar/mysql/5.6.27/bin/mysql
 
-connect_sql="mysql -h $HOSTNAME -P $PORT -u $USERNAME -p$PASSWORD"
+connect_sql="$MYSQL -h $HOSTNAME -P $PORT -u $USERNAME -p$PASSWORD"
 $connect_sql -e "drop database $DATABASE"
 $connect_sql -e "create database $DATABASE"
 $connect_sql $DATABASE < create_yogy.sql
 
 
 echo "Initial population of data"
-perl ${PERL_DIR}/update_database.pl $HOSTNAME $PORT $DATABASE $USERNAME $PASSWORD 
+#perl ${PERL_DIR}/update_database.pl $HOSTNAME $PORT $DATABASE $USERNAME $PASSWORD 
 
-
+echo "Adding Inparanoid"
+paranoid_path='/Users/sinan/workspace/bahler/inparanoid.sbc.su.se/download/current/Orthologs_other_formats/inparout'
+paranoid_file=($(ls $paranoid_path))
+for(( i=0; i<${#paranoid_file[@]}; i++ ))
+do
+  perl ${PERL_DIR}/yogy_add_inp_terms.pl ${paranoid_path}/${paranoid_file[i]}\
+  $MYSQL $DATABASE $HOSTNAME $PORT $USERNAME $PASSWORD
+  printf '\r           \r'$i/${#paranoid_file[@]} ,  
+done
