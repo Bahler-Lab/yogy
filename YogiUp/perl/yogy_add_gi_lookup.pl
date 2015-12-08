008da9d7a6dbfd71723400afcc24e32b03fcf62d
root <root@bahlerweb.cs.ucl.ac.uk>
2015-12-08 18:50:52 +0000

    add GI lookup table and uniprot lookup table

--- YogiUp/perl/yogy_add_gi_lookup.pl
+++ YogiUp/perl/yogy_add_gi_lookup.pl
@@ -4,80 +4,58 @@ use strict;
 use Data::Dumper;
 use DBI;
 
-#my $file = shift;
-my $file = "gene2accession";
-my $dbh = connect_to_DB('mysql','S_pombe_YOGY_3','128.40.79.33','3306','','yogyrw','yogyex');
+my ($file, $MYSQL, $DATABASE, $HOST, $PORT, $USER, $PASSWD) = @ARGV;
+my $dbh = connect_to_DB($MYSQL, $DATABASE, $HOST, $PORT, '', $USER, $PASSWD);
 
 
 open(GI, $file)
     or die "Couldn't open file $file: $!";
-
 while (defined (my $line = <GI>) ) {
-
     chomp $line;
-
-    #print "[$line]\n";
-
     insert_gi_terms($line);
-
 }
-
 close(GI);
-
 disconnect_from_DB();
 
 
 sub connect_to_DB {
-
     my ($driver, $instance, $host, $port, $cnf_file, $user, $password) = @_;
-
     my $dbh = DBI->connect("DBI:${driver}:database=${instance};host=${host};port=$port;mysql_read_default_file=${cnf_file}",$user,$password)
         or die "ERROR: $DBI::errstr\n";
-
     if (!defined $dbh) {
         die "Could not connect to database: $!";
     }
-
     return $dbh;
 }
 
 
 sub disconnect_from_DB {
-
     $dbh->disconnect()
         or die "Can't disconnect: $DBI::errstr\n";
-
 }
 
 
 sub insert_gi_terms {
-
     my $line = shift;
-
     my @array = split /\t/, $line;
-
-   #print "$array[0] \t$array[1] \t$array[5]\t $array[6]\n";
-
-    my $insert = qq(INSERT INTO gi_lookup (tax_id,
-                                           gene_id,
-                                           prot_acc,
-                                           prot_id)
-                    VALUES (?, ?, ?, ?)
-                   );
+    my $insert = qq(INSERT IGNORE INTO gi_lookup (tax_id,
+    gene_id,
+    prot_acc,
+    prot_id)
+    VALUES (?, ?, ?, ?)
+    );
 
     my $sth = $dbh->prepare($insert)
         or die "Can't prepare: $DBI::errstr\n";
 
     $array[1] = '-'
-        if not $array[1];
+    if not $array[1];
 
     $array[6] = '-'
-        if not $array[6];
+    if not $array[6];
 
     $sth->execute($array[0],
-                  $array[1],
-                  $array[5],
-                  $array[6]);
-        #or warn "Can't execute: $DBI::errstr\n";
-
+        $array[1],
+        $array[5],
+        $array[6]);
 }
--- YogiUp/perl/yogy_add_uniprot_lookup.pl
+++ YogiUp/perl/yogy_add_uniprot_lookup.pl
@@ -4,63 +4,42 @@ use strict;
 use Data::Dumper;
 use DBI;
 
-my $file = shift;
-
-my $dbh = connect_to_DB('mysql','S_pombe_YOGY_3','128.40.79.33','3306','','yogyrw','yogyex');
-
+my ($file, $MYSQL, $DATABASE, $HOST, $PORT, $USER, $PASSWD) = @ARGV;
+my $dbh = connect_to_DB($MYSQL, $DATABASE, $HOST, $PORT, '', $USER, $PASSWD);
 
 open(UNI, $file)
     or die "Couldn't open file $file: $!";
-
 while (defined (my $line = <UNI>) ) {
-
     chomp $line;
-
-    #print "[$line]\n";
-
     insert_uni_terms($line);
-
 }
-
 close(UNI);
-
 disconnect_from_DB();
 
 
 sub connect_to_DB {
-
     my ($driver, $instance, $host, $port, $cnf_file, $user, $password) = @_;
-
     my $dbh = DBI->connect("DBI:${driver}:database=${instance};host=${host};port=$port;mysql_read_default_file=${cnf_file}",$user,$password)
         or die "ERROR: $DBI::errstr\n";
-
     if (!defined $dbh) {
         die "Could not connect to database: $!";
     }
-
     return $dbh;
 }
 
 
 sub disconnect_from_DB {
-
     $dbh->disconnect()
         or die "Can't disconnect: $DBI::errstr\n";
-
 }
 
 
 sub insert_uni_terms {
-
     my $line = shift;
-
     my @array = split /\t/, $line;
-
     my @accs = split /;/, $array[1];
-
     foreach my $acc (@accs) {
-
-        my $insert = qq(INSERT INTO uniprot_lookup (uni_name,
+        my $insert = qq(INSERT IGNORE INTO uniprot_lookup (uni_name,
                                                     pri_uni_acc,
                                                     uni_acc,
                                                     gb_dna_acc,
@@ -77,7 +56,5 @@ sub insert_uni_terms {
                       $array[2],
                       $array[3])
             or warn "Can't execute: $DBI::errstr\n";
-
     }
-
 }
