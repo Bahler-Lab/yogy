9fc56cb06ad7ca8b282d93523dcca57bd179aeef
Sinan SHi <s.shi@ucl.ac.uk>
2016-01-26 16:13:02 +0000

    refactoring download_inparanoid; adding inparanoid selection

--- YogiUp/download_inparanoid.sh
+++ YogiUp/download_inparanoid.sh
@@ -1,8 +1,75 @@
 source settings.sh
 INPAR_DIR="$TEMP_DIR/inparanoid"
+# the web location
+INPAR_LOCATION="$INPAR_DIR/inparanoid.sbc.su.se/download/8.0_current/Orthologs_other_formats" 
+
+INPAR_SELECT_DIR="$TEMP_DIR/inparanoid_select_species"
 DATE_FILE="inparanoid_update_date.txt"
 
+#############################################
+# check if the download is old enough (4 months)
+# Arguments:
+#   None
+# Returns:
+#   update_tag - 1: update required 0: update not required
+#############################################
+check_update()
+{
+    if [ ! -d ${INPAR_DIR} ]; then
+        mkdir ${INPAR_DIR}
+    fi
 
+    # check update if the update is the latest.
+    if [ -e "$INPAR_DIR/$DATE_FILE" ]; then
+        update_date=$(cat $INPAR_DIR/$DATE_FILE)
+        today=$(date +%Y%m)
+        monthdiff=$(( $today-$update_date ))
+        if [ $monthdiff -lt 4 ]; then
+            update_tag=0
+        else
+            update_tag=1
+        fi
+    else
+        update_tag=1
+    fi
+    echo $update_tag
+}
+
+#############################################
+# download the inparanoid files write a $DATE_FILE
+# to record the date
+# Arguments:
+#   update_tag
+# Global:
+#   inparanoid_link: specified setting.sh
+#   DATE_FILE
+#   INPAR_DIR
+# Returns:
+#   None
+#############################################
+download_update()
+{
+    update_tag=$1
+    printf "downloading %-40s\n " "inparanoid orthologs  (full download takes several hours):"
+    if [ $update_tag -eq 1 ]; then 
+        cd ${INPAR_DIR} # switch to temp directory
+        wget -q -r --no-parent --reject "index.htm*" $inparanoid_link
+        echo $(date +%Y%m) > $DATE_FILE
+        cd ../../
+    else
+        printf "\e[0;33m[is the latest: %s]\e[0m\n" "$(cat $INPAR_DIR/$DATE_FILE)"
+    fi
+}
+
+#############################################
+# Extract all files from the downloading folder
+# Arguments:
+#   1: inparanoid download directory
+#   2: extract destination
+#   3: update tag (returned by check_update())
+# Returns:
+#   None
+#############################################
 extract_inparanoid () 
 {
     update_tag=$3
@@ -10,7 +77,7 @@ extract_inparanoid ()
     if [ ! -d $2 ];then
         mkdir $2
     fi
-    if [ $update_tag == 1 ];then
+    if [ $update_tag -eq "1" ];then
         echo "extracting ${#files[@]} orthologs ... " 
 
         for (( i=0; i<${#files[@]}; i++))
@@ -18,7 +85,8 @@ extract_inparanoid ()
             tar -xzf ${files[$i]} -C $2 sqltable.*
             printf '\r           \r'$i/${#files[@]} ,  
         done
-        if [ $i == ${#file[@]} ];then
+        
+        if [ $i == ${#files[@]} ];then
             $DONE
         else
             $FAILED
@@ -26,35 +94,32 @@ extract_inparanoid ()
     fi
 }
 
-
-
-if [ ! -d ${INPAR_DIR} ]; then
-    mkdir ${INPAR_DIR}
-fi
-
-# check update if the update is the latest.
-if [ -e "$INPAR_DIR/$DATE_FILE" ]; then
-    update_date=$(cat $INPAR_DIR/$DATE_FILE)
-    today=$(date +%Y%m)
-    monthdiff=$(( $today-$update_date ))
-    if [ $monthdiff -lt 4 ]; then
-        update_tag=0
-    else
-        update_tag=1
+#############################################
+# copy the selected species to a new directory
+# Global:
+#   inparanoid_species: array of selected species
+#   INPAR_SELECT_DIR
+#   INPAR_LOCATION
+#############################################
+select_species()
+{
+    if [ ! -d $INPAR_SELECT_DIR ]; then
+        mkdir $INPAR_SELECT_DIR
     fi
-else
-    update_tag=1
-fi
+
+    for (( i=0; i<${#inparanoid_species[@]}; i++ ))
+    do
+        copy_file=$INPAR_LOCATION/${inparanoid_species[i]}
+        if [ -d $copy_file ]; then
+            cp -r $copy_file $INPAR_SELECT_DIR/
+        else
+            $FAILED
+        fi
+    done
+}
 
 
-printf "downloading %-40s\n " "inparanoid orthologs  (full download takes several hours):"
-if [ $update_tag == 1 ]; then
-    cd ${INPAR_DIR} # switch to temp directory
-    wget -q -r --no-parent --reject "index.htm*" $inparnoid_link
-    echo $(date +%Y%m) > $DATE_FILE
-    cd ../../
-else
-    printf "\e[0;33m[is the latest: %s]\e[0m\n" "$(cat $INPAR_DIR/$DATE_FILE)"
-fi
 
-extract_inparanoid $INPAR_DIR $DATA_DIR/inparanoid $update_tag
+download_update $(check_update)
+#select_species
+extract_inparanoid $INPAR_SELECT_DIR $DATA_DIR/inparanoid $(check_update)
